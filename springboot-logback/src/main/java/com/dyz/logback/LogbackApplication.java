af1ea11b9d455a42500902e964228d9fd4bafa1f
Michael <42629466+MichaelDYZ@users.noreply.github.com>
2020-05-10 21:29:30 +0800

    Add files via upload

---
+++ springboot-logback/src/main/java/com/dyz/logback/LogbackApplication.java
@@ -0,0 +1,35 @@
+package com.dyz.logback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ * @author dyz
+ * @version 1.0
+ * @date 2020/5/8 15:20
+ */
+@SpringBootApplication
+//Slf4j需要引入lombok
+@Slf4j
+public class LogbackApplication {
+
+    public static void main(String[] args) {
+      ConfigurableApplicationContext  context = SpringApplication.run(LogbackApplication.class, args);
+        int length = context.getBeanDefinitionNames().length;
+        log.trace("Spring boot启动初始化了 {} 个 Bean", length);
+        log.debug("Spring boot启动初始化了 {} 个 Bean", length);
+        log.info("Spring boot启动初始化了 {} 个 Bean", length);
+        log.warn("Spring boot启动初始化了 {} 个 Bean", length);
+        log.error("Spring boot启动初始化了 {} 个 Bean", length);
+        try {
+            int i = 0;
+            int j = 1 / i;
+        } catch (Exception e) {
+            log.error("【SpringBootDemoLogbackApplication】启动异常：", e);
+        }
+
+    }
+
+}
